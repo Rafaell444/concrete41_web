8a007e04150aa2fbfa92e11d842a4e0906c61ce9
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-10-01 16:32:31 +0400

    commit3

--- scripts/flowerpot.js
+++ scripts/flowerpot.js
@@ -1,34 +1,15 @@
 const apiResult = [{
     title:"qotanski",
-    Color: "['#111','#222']",
+    Color1: "#111",
+    Color2: "yellow",
+    Color3: "",
+    Color4: "",
+    Color5: "",
+    Color6: "",
+    Color7: "",
     Price: "12",
     Code: "pot1",
     Size1: "20",
     Size2: "30",
     Size3: "33"
-  }, {
-    title:"qotanski",
-    Color: "🔴",
-    Price: "20",
-    Code: "pot2",
-    Size1: "out1",
-    Size2: "out1",
-    Size3: "out1"
-  }, {
-    title:"qotanski",
-    Color: "🔴",
-    Price: "15",
-    Code: "pot3",
-    Size1: "out1",
-    Size2: "out1",
-    Size3: "out1"
-  },
-  {
-    title:"qotanski",
-    Color: "🔴",
-    Price: "15",
-    Code: "pot3",
-    Size1: "out1",
-    Size2: "out1",
-    Size3: "out1"
-  }];
+  },];
